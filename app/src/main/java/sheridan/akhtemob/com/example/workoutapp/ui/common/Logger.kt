fce112d7a9b8dfd57312e5c58855e9548b120011
mobin <mobinakhter18@gmail.com>
2020-12-06 17:06:09 -0500

    Added a Log.i to the Logger.kt object class.

--- app/src/main/java/sheridan/akhtemob/com/example/workoutapp/ui/common/Logger.kt
+++ app/src/main/java/sheridan/akhtemob/com/example/workoutapp/ui/common/Logger.kt
@@ -1,4 +1,15 @@
 package sheridan.akhtemob.com.example.workoutapp.ui.common
 
+import android.util.Log
+
 object Logger {
+
+    // Declarations
+
+    private const val generalTag = "App"
+    private const val emptyText = "Given Text is Empty"
+
+    fun info(text: String?, tag: String = generalTag) {
+        Log.i(tag, text?: emptyText)
+    }
 }
