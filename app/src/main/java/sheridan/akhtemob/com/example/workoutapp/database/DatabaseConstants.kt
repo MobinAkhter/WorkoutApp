7933815e9ea82fcde4ed4d800490ab965fd0353b
mobin <mobinakhter18@gmail.com>
2020-12-06 15:37:14 -0500

    Declared and defined three const val in DatabaseConstants.

---
+++ app/src/main/java/sheridan/akhtemob/com/example/workoutapp/database/DatabaseConstants.kt
@@ -0,0 +1,8 @@
+package sheridan.akhtemob.com.example.workoutapp.database
+
+object DatabaseConstants {
+    // const val declarations
+    const val databaseName = "example.workoutapp.database.v1"
+    const val tableJoggingEntries = "tableJoggingEntries"
+    const val tableFreeWeightsEntries = "tableFreeWeightsEntries"
+}
