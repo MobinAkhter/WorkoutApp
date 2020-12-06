56bfdbbe646b1c60d912ea1cb7cd2c50cff419ee
mobin <mobinakhter18@gmail.com>
2020-12-06 15:26:02 -0500

    Edited the build.gradle file

--- app/src/main/java/sheridan/akhtemob/com/example/workoutapp/ui/activities/MainActivity.kt
+++ app/src/main/java/sheridan/akhtemob/com/example/workoutapp/ui/activities/MainActivity.kt
@@ -4,7 +4,30 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import sheridan.akhtemob.com.example.workoutapp.R
 
 
 class MainActivity AppCompatActivity(){
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+        navController = navHostFragment.navController
+
+        NavigationUI.setupActionBarWithNavController(this, navController)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        navController.navigateUp()
+        return super.onSupportNavigateUp()
+    }
 }
