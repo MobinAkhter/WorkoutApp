422904cd51755c32dd55fe8dca3d7bc1997a4e3b
mobin <mobinakhter18@gmail.com>
2020-12-06 16:57:08 -0500

    Added code to the Dialogs.kt file.

--- app/src/main/java/sheridan/akhtemob/com/example/workoutapp/ui/common/Dialogs.kt
+++ app/src/main/java/sheridan/akhtemob/com/example/workoutapp/ui/common/Dialogs.kt
@@ -1,4 +1,103 @@
 package sheridan.akhtemob.com.example.workoutapp.ui.common
 
+import android.app.Activity
+import android.app.AlertDialog
+import android.content.Context
+import android.widget.Toast
+
 object Dialogs {
+    fun showMessage(
+        activity: Activity,
+        message: String,
+        positiveButtonText: String,
+        listener: AlertDialogInterface
+    ) {
+
+        val builder = AlertDialog.Builder(activity)
+        builder.setMessage(message)
+        builder.setPositiveButton(positiveButtonText) { dialogInterface, i ->
+            listener.positiveButtonClick()
+        }
+
+        val alert = builder.create()
+        alert.show()
+    }
+
+    fun showMessage(
+        context: Context,
+        message: String,
+        positiveButtonText: String,
+        negativeButtonText: String,
+        listener: AlertDialogInterface
+    ) {
+        val builder = AlertDialog.Builder(context)
+        builder.setMessage(message)
+        builder.setPositiveButton(positiveButtonText) { dialogInterface, i ->
+            listener.positiveButtonClick()
+        }
+        builder.setNegativeButton(negativeButtonText) { dialogInterface, i ->
+            listener.negativeButtonClick()
+        }
+
+        val alert = builder.create()
+        alert.show()
+    }
+
+    fun showMessage(
+        activity: Activity,
+        message: String,
+        positiveButtonText: String,
+        negativeButtonText: String,
+        listener: AlertDialogInterface
+    ) {
+        val builder = AlertDialog.Builder(activity)
+        builder.setMessage(message)
+        builder.setPositiveButton(positiveButtonText) { dialogInterface, i ->
+            listener.positiveButtonClick()
+        }
+        builder.setNegativeButton(negativeButtonText) { dialogInterface, i ->
+            listener.negativeButtonClick()
+        }
+
+        val alert = builder.create()
+        alert.show()
+    }
+
+    fun showMessage(
+        context: Context,
+        message: String
+    ) {
+
+        val builder = AlertDialog.Builder(context)
+        builder.setMessage(message)
+        builder.setPositiveButton(R.string.ok) { dialogInterface, i ->
+            dialogInterface.dismiss()
+        }
+
+        val alert = builder.create()
+        alert.show()
+    }
+
+    fun showToast(context: Context, message: String) {
+        Toast.makeText(context, message, Toast.LENGTH_LONG).show()
+    }
+
+    fun showToast(activity: Activity, message: String) {
+        Toast.makeText(activity, message, Toast.LENGTH_LONG).show()
+    }
+
+    fun showMessage(
+        activity: Activity,
+        message: String
+    ) {
+        val builder = AlertDialog.Builder(activity)
+        builder.setMessage(message)
+        builder.setPositiveButton(R.string.ok) { dialogInterface, i ->
+            dialogInterface.dismiss()
+        }
+
+        val alert = builder.create()
+        alert.show()
+    }
+
 }
