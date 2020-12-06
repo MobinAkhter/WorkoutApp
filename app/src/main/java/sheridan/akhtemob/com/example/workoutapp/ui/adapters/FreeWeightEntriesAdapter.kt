1ce96bb5f4543ad40954204bd745f852c963d0f7
mobin <mobinakhter18@gmail.com>
2020-12-06 16:29:12 -0500

    Added code to FreeWeightEntriesAdapter.kt. A bind fun, oncreateVieeHolder fun, fun onBindViewHolder, and fun getItemCunt()

--- app/src/main/java/sheridan/akhtemob/com/example/workoutapp/ui/adapters/FreeWeightEntriesAdapter.kt
+++ app/src/main/java/sheridan/akhtemob/com/example/workoutapp/ui/adapters/FreeWeightEntriesAdapter.kt
@@ -12,5 +12,35 @@ class FreeWeightEntriesAdapter (
     private val list: ArrayList<FreeWeightsEntryModel>,
     private val listener: AdapterClickListener
 ) : RecyclerView.Adapter<FreeWeightEntriesAdapter.ViewClass>() {
+    var context: Context? = null
 
+    inner class ViewClass(val binding: SingleRowFreeWeightEntryBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(model: FreeWeightsEntryModel) {
+            binding.model = model
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewClass {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding: SingleRowFreeWeightEntryBinding =
+            inflate(layoutInflater, R.layout.single_row_free_weight_entry, parent, false)
+        context = parent.context
+        return ViewClass(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewClass, position: Int) {
+        holder.bind(list[position])
+        holder.binding.date.text = Store.getReadableDate(list[position].date)
+        holder.binding.time.text = Store.getReadableTime(list[position].time)
+        holder.binding.parentLayout.setOnClickListener {
+            listener.click(list[position].id)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
 }
