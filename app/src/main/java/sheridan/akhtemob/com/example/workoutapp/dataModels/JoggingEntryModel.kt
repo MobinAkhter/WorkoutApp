9b84dd9c46aab2d1c214d1ea07a970a6b022234d
mobin <mobinakhter18@gmail.com>
2020-12-06 16:19:25 -0500

    Added the declaration of values that will be used in the jogging exercise in the JoggingEntryModel.kt.

--- app/src/main/java/sheridan/akhtemob/com/example/workoutapp/dataModels/JoggingEntryModel.kt
+++ app/src/main/java/sheridan/akhtemob/com/example/workoutapp/dataModels/JoggingEntryModel.kt
@@ -1,5 +1,16 @@
 package sheridan.akhtemob.com.example.workoutapp.dataModels
 
-class JoggingEntryModel {
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import sheridan.akhtemob.com.example.workoutapp.database.DatabaseConstants
 
+@Entity(tableName = DatabaseConstants.tableJoggingEntries)
+data class JoggingEntryModel(
+    val date: Long,
+    val time: Long,
+    val distance: Double,
+    val distanceUnit: String
+){
+    @PrimaryKey(autoGenerate = true)
+    var id: Int = 0
 }
