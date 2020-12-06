47c72d416c78c88b8787c2eac4ad9c448610348a
mobin <mobinakhter18@gmail.com>
2020-12-06 16:14:25 -0500

    Added the declaration of values that will be used in the weights exercise.

--- app/src/main/java/sheridan/akhtemob/com/example/workoutapp/dataModels/FreeWeightsEntryModel.kt
+++ app/src/main/java/sheridan/akhtemob/com/example/workoutapp/dataModels/FreeWeightsEntryModel.kt
@@ -1,4 +1,17 @@
 package sheridan.akhtemob.com.example.workoutapp.dataModels
 
-class FreeWeightsEntryModel {
+import sheridan.akhtemob.com.example.workoutapp.database.DatabaseConstants
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = DatabaseConstants.tableFreeWeightsEntries)
+data class FreeWeightsEntryModel(
+    val date: Long,
+    val time: Long,
+    val reps: Int,
+    val sets: Int,
+    val weight: Double
+) {
+    @PrimaryKey(autoGenerate = true)
+    var id: Int = 0
 }
