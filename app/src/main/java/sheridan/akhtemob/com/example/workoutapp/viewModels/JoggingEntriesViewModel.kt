0d0d7954bb0bf6294b5efe788bc8c31800cf60a1
mobin <mobinakhter18@gmail.com>
2020-12-06 17:28:49 -0500

    Added code inside JoggingEntriesViewModel.kt, and import statements.

--- app/src/main/java/sheridan/akhtemob/com/example/workoutapp/viewModels/JoggingEntriesViewModel.kt
+++ app/src/main/java/sheridan/akhtemob/com/example/workoutapp/viewModels/JoggingEntriesViewModel.kt
@@ -1,4 +1,37 @@
 package sheridan.akhtemob.com.example.workoutapp.viewModels
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import sheridan.akhtemob.com.example.workoutapp.dataModels.JoggingEntryModel
+import sheridan.akhtemob.com.example.workoutapp.database.LocalRepositoryDao
+import sheridan.akhtemob.com.example.workoutapp.database.LocalRepositoryImpl
+
 class JoggingEntriesViewModel {
+
+    private val localRepository: LocalRepositoryDao = LocalRepositoryImpl.get()
+
+    fun getAllJoggingEntries() = localRepository.getAllJoggingEntries()
+
+    fun addJoggingEntry(model: JoggingEntryModel) {
+        localRepository.addJoggingEntry(model)
+    }
+
+    fun getJoggingEntryById(id: Int): LiveData<JoggingEntryModel?> {
+        return localRepository.getJoggingEntryById(id)
+    }
+
+    fun deleteJoggingEntry(id: Int) {
+        localRepository.deleteJoggingEntry(id)
+    }
+
+    fun updateJoggingEntry(
+        date: Long,
+        time: Long,
+        distance: Double,
+        distanceUnit: String,
+        id: Int
+    ) {
+        localRepository.updateJoggingEntry(date, time, distance, distanceUnit, id)
+    }
+
 }
