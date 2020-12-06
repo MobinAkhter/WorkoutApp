d8b1cfd48536bb9908d65e31137571f63e073c2e
mobin <mobinakhter18@gmail.com>
2020-12-06 15:43:24 -0500

    Added code to LocalDatabase.kt. There are a few errors, but they will be solved shortly.

--- app/src/main/java/sheridan/akhtemob/com/example/workoutapp/database/LocalDatabase.kt
+++ app/src/main/java/sheridan/akhtemob/com/example/workoutapp/database/LocalDatabase.kt
@@ -1,4 +1,16 @@
 package sheridan.akhtemob.com.example.workoutapp.database
 
-class LocalDatabase {
+import sheridan.akhtemob.com.example.workoutapp.dataModels.FreeWeightsEntryModel
+import sheridan.akhtemob.com.example.workoutapp.dataModels.JoggingEntryModel
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(
+    entities = [JoggingEntryModel::class, FreeWeightsEntryModel::class],
+    version = 1,
+    exportSchema = false
+)
+
+abstract class LocalDatabase {
+    abstract fun getDao(): LocalRepositoryDao
 }
